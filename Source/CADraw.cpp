a3cadeb04bc63b117b255a00e822ad6fe099a13c
El-Wray <elwray.ru@gmail.com>
2014-08-22 15:48:56 +0300

    Commit.

--- Source/CADraw.cpp
+++ Source/CADraw.cpp
@@ -1,18 +1,14 @@
 #include "CADraw.h"
 #include "Constants.h"
 
-SCADrawInitResult g_sCADRaw;
+SCADrawInitResult	g_sCADRaw = { 0, };
 
-LPDIRECTDRAW g_lpDirectDraw = NULL;
-LPDIRECTDRAWSURFACE g_lpSurface = NULL;
+LPDIRECTDRAW		g_lpDirectDraw = NULL;
+LPDIRECTDRAWSURFACE	g_lpSurface = NULL;
+LPVOID				g_pSurfaceData = NULL;
 
-HWND g_hWnd = NULL;
-BOOL g_bFullscreen = FALSE;
-
-SCADrawInitResult* CADrawInit()
-{
-	return &g_sCADRaw;
-}
+HWND g_hWnd			= NULL;
+BOOL g_bFullscreen	= FALSE;
 
 void SetScreenVariables()
 {
@@ -121,3 +117,137 @@ BOOL SetDisplayMode(DWORD dwWidth, DWORD dwHeight)
 
 	return 1;
 }
+
+//	x_sub_10001430
+
+//	x_sub_100014C0
+
+//	x_sub_10001580_call4
+
+//	sub_100015E0
+
+//	sub_100016D0
+
+//	x_sub_100017F0
+
+//	x_sub_10001850
+
+//	sub_100018B0
+
+//	j_nullsub_1
+
+//	nullsub_1
+
+SCADrawInitResult* CADrawInit()
+{
+	return &g_sCADRaw;
+}
+
+//	x_sub_10001BF0
+
+//	x_sub_10001C80
+
+//	x_sub_10001D00
+
+//	x_sub_10001EA0_call
+
+//	x_sub_10001EE0_call
+
+//	x_sub_10001F20_call
+
+//	x_sub_10001F50_call
+
+//	sub_10001F90
+
+//	sub_10002030
+
+//	sub_100024C0
+
+//	sub_100027C0
+
+//	sub_10002860
+
+//	sub_100028F0
+
+BOOL LockSurface()
+{
+	return FALSE;
+}
+
+BOOL UnlockSurface()
+{
+	return FALSE;
+}
+
+BOOL CopyDataToDirectDrawSurface()
+{
+	return FALSE;
+}
+
+//	sub_10002B10
+
+//	sub_10002B70
+
+//	sub_10002C70
+
+//	sub_10003090
+
+//	x_sub_10003400
+
+//	x_sub_10003430
+
+//	x_sub_10003490
+
+//	h_________________sub_100034F0
+
+//	sub_100038EE
+
+//	sub_10003D18
+
+//	sub_100040E6
+
+//	sub_10004460
+
+//	sub_10004786
+
+//	sub_10004AB6
+
+//	sub_10004E80
+
+//	sub_100051AF
+
+//	sub_10005493
+
+//	sub_1000586C
+
+//	sub_10005B96
+
+//	sub_10005F01
+
+//	sub_1000625D
+
+//	sub_10006586
+
+//	sub_1000687D
+
+//	sub_10006C48
+
+//	sub_10006FE2
+
+//	sub_100073B2
+
+//	sub_10007678
+
+//	sub_10007938
+
+//	sub_10007D0C
+
+//	x_sub_100087A1
+
+//	sub_100088E9
+
+//	sub_100095A8
+
+//	sub_100098D3
+
+//	sub_10009F13
