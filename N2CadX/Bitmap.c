ecacc25f2665c83a59f51b57b8092d71003f6fde
Dmitry (ElWray) Ermakov <elwray.ru@gmail.com>
2016-03-30 16:51:50 +0300

    * Bitmap functions was renaimed. * LoadBitmap16 and SaveBitmap16 functions was done. * Memory allocation method was changed from c++ style (new keyword) to c-style with malloc/free methods.

--- N2CadX/Bitmap.c
+++ N2CadX/Bitmap.c
@@ -1,10 +1,11 @@
 #include <assert.h>
+#include <stdlib.h>
 #include "Bitmap.h"
 #include "Constants.h"
 
 
 // https://msdn.microsoft.com/ru-ru/library/windows/desktop/dd183402(v=vs.85).aspx
-SBitmap16* CreateBitmap(INT width, INT height)
+SBitmap16* CreateBitmap16(INT width, INT height)
 {
 	assert(width > 0);
 	if (width <= 0)
@@ -13,15 +14,15 @@ SBitmap16* CreateBitmap(INT width, INT height)
 	if (height <= 0)
 		return NULL;
 
-	SBitmap16* p_bitmap = new SBitmap16;
-	p_bitmap->p_data = new WORD[width * height];
+	SBitmap16* p_bitmap = (SBitmap16*) calloc(1, sizeof(SBitmap16));
+	p_bitmap->p_data = (WORD*) calloc(width * height, sizeof(DWORD));
 	p_bitmap->width = width;
 	p_bitmap->height = height;
 
 	return p_bitmap;
 }
 
-BOOL SaveBitmap(SBitmap16* p_bitmap, LPCWSTR p_path)
+BOOL SaveBitmap16(SBitmap16* p_bitmap, LPCWSTR p_path)
 {
 	assert(p_bitmap);
 	if (!p_bitmap)
@@ -30,8 +31,19 @@ BOOL SaveBitmap(SBitmap16* p_bitmap, LPCWSTR p_path)
 	if (!p_path || !*p_path)
 		return FALSE;
 
+	HANDLE handle = CreateFile(p_path, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (handle == INVALID_HANDLE_VALUE)
+		return FALSE;
+
+	auto dataSize = sizeof(WORD) * p_bitmap->width * p_bitmap->height;
+
+	BITMAPFILEHEADER file = { 0, };
+	file.bfType = 0x4D42;
+	file.bfSize = (DWORD)(sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFO)) + dataSize;
+	file.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFO);
+
 	BITMAPINFO info = { 0, };
-	info.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	info.bmiHeader.biSize = sizeof(BITMAPINFO);
 	info.bmiHeader.biWidth = p_bitmap->width;
 	info.bmiHeader.biHeight = p_bitmap->height;
 	info.bmiHeader.biPlanes = 1;
@@ -41,20 +53,68 @@ BOOL SaveBitmap(SBitmap16* p_bitmap, LPCWSTR p_path)
 	info.bmiColors[0].rgbGreen = GreenMaskDWORD;
 	info.bmiColors[0].rgbBlue = BlueMaskDWORD;
 
-	BITMAPFILEHEADER file = { 0, };
+	DWORD bytesWritten;
+	WriteFile(handle, &file, sizeof(BITMAPFILEHEADER), &bytesWritten, NULL);
+	WriteFile(handle, &info, sizeof(BITMAPINFO), &bytesWritten, NULL);
+	WriteFile(handle, p_bitmap->p_data, dataSize, &bytesWritten, NULL);
+
+	CloseHandle(handle);
 
 	return FALSE;
 }
 
-BOOL SetPixel(SBitmap16* p_bitmap, INT x, INT y, WORD color)
+SBitmap16* LoadBitmap16(LPCWSTR p_path)
+{
+	assert(p_path && *p_path);
+	if (!p_path && !*p_path)
+		return NULL;
+
+	auto handle = CreateFile(p_path, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (handle == INVALID_HANDLE_VALUE)
+		return NULL;
+
+	DWORD bytesRead;
+
+	BITMAPFILEHEADER file = { 0, };
+	if (!ReadFile(handle, &file, sizeof(BITMAPFILEHEADER), &bytesRead, NULL))
+	{
+		CloseHandle(handle);
+		return NULL;
+	}
+
+	BITMAPINFO info = { 0, };
+	if (ReadFile(handle, &info, sizeof(BITMAPINFO), &bytesRead, NULL))
+	{
+		CloseHandle(handle);
+		return NULL;
+	}
+
+	auto dataSize = info.bmiHeader.biWidth * info.bmiHeader.biHeight;
+	WORD* p_data = calloc(dataSize, sizeof(WORD));
+	if (!ReadFile(handle, p_data, dataSize * sizeof(WORD), &bytesRead, NULL))
+	{
+		CloseHandle(handle);
+		free(p_data);
+		return NULL;
+	}
+
+	SBitmap16* p_bitmap = (SBitmap16*) calloc(1, sizeof(SBitmap16));
+	p_bitmap->p_data = p_data;
+	p_bitmap->width = info.bmiHeader.biWidth;
+	p_bitmap->height = info.bmiHeader.biHeight;
+
+	return p_bitmap;
+}
+
+BOOL SetPixel16(SBitmap16* p_bitmap, INT x, INT y, WORD color)
 {
 	assert(p_bitmap);
 	if (!p_bitmap)
 		return FALSE;
-	assert(x >= 0 && x < p_bitmap.width);
+	assert(x >= 0 && x < p_bitmap->width);
 	if (x < 0 || x >= p_bitmap->width)
 		return FALSE;
-	assert(y >= 0 && y < p_bitmap.height);
+	assert(y >= 0 && y < p_bitmap->height);
 	if (y < 0 || y >= p_bitmap->height)
 		return FALSE;
 
@@ -63,19 +123,19 @@ BOOL SetPixel(SBitmap16* p_bitmap, INT x, INT y, WORD color)
 	return TRUE;
 }
 
-BOOL DeleteBitmap(SBitmap16* p_bitmap)
+BOOL DeleteBitmap16(SBitmap16* p_bitmap)
 {
 	assert(p_bitmap);
 	if (!p_bitmap)
 		return;
 
 	if (p_bitmap->p_data)
-		delete[] p_bitmap.p_data;
+		free(p_bitmap->p_data);
 
-	delete p_bitmap;
+	free(p_bitmap);
 }
 
-BOOL SaveBitmapData(INT width, INT height, WORD* p_data, LPCWSTR p_path)
+BOOL SaveBitmap16Data(INT width, INT height, WORD* p_data, LPCWSTR p_path)
 {
 	assert(width > 0);
 	if (width <= 0)
@@ -87,5 +147,10 @@ BOOL SaveBitmapData(INT width, INT height, WORD* p_data, LPCWSTR p_path)
 	if (!p_data)
 		return FALSE;
 
-	return FALSE;
+	SBitmap16* p_bitmap = CreateBitmap16(width, height);
+	memcpy(p_bitmap->p_data, p_data, sizeof(WORD) * width * height);
+	auto result = SaveBitmap(p_bitmap, p_path);
+	DeleteBitmap(p_bitmap);
+
+	return result;
 }
--- N2CadX/Bitmap.h
+++ N2CadX/Bitmap.h
@@ -15,10 +15,11 @@ struct _SBitmap16
 typedef struct _SBitmap16 SBitmap16;
 
 
-SBitmap16* CreateBitmap(INT width, INT height);
-BOOL SaveBitmap(SBitmap16* p_bitmap, LPCWSTR p_path);
-BOOL SetPixel(SBitmap16* p_bitmap, INT x, INT y, WORD color);
-BOOL DeleteBitmap(SBitmap16* p_bitmap);
-BOOL SaveBitmapData(INT width, INT height, WORD* p_data, LPCWSTR p_path);
+SBitmap16* CreateBitmap16(INT width, INT height);
+BOOL SaveBitmap16(SBitmap16* p_bitmap, LPCWSTR p_path);
+SBitmap16* LoadBitmap16(LPCWSTR p_path);
+BOOL SetPixel16(SBitmap16* p_bitmap, INT x, INT y, WORD color);
+BOOL DeleteBitmap16(SBitmap16* p_bitmap);
+BOOL SaveBitmap16Data(INT width, INT height, WORD* p_data, LPCWSTR p_path);
 
 #endif // BITMAP_H
